11b3b6cdc5e87fa1eb64e0a163f7bf43541d767f
hangerGit <hangcity100@gmail.com>
2024-12-17 20:56:17 +0800

    增加checkbox功能

--- myJS.js
+++ myJS.js
@@ -1,16 +1,19 @@
 
 // 打开或创建数据库
-var request = indexedDB.open('stackDB', 1); //myDataBase    
+var request = indexedDB.open('stackDB',3); //myDataBase    
 var db;
+var storeName='customers3';
 
 // 如果数据库版本变化或首次创建时触发
 request.onupgradeneeded = function(event) {
     console.log("没有發現資料庫或版本不一樣....");
     var db = event.target.result;
     // 创建对象存储（表），设置主键为 'id'
-    var objectStore = db.createObjectStore('customers', { keyPath: 'id' });
+    //var objectStore = db.createObjectStore('customers', { keyPath: 'id' });
+    var objectStore = db.createObjectStore(storeName, { keyPath:"id" , autoIncrement:true});         
     // 为 'name' 字段创建索引
     objectStore.createIndex('name', 'name', { unique: false });
+    objectStore.createIndex('id', 'id', { unique: true });
 };
 
 // 打开数据库成功
@@ -18,8 +21,9 @@ request.onsuccess = function(event) {
   console.log("開檔成功");   
   db=event.target.result;
 
+  clearTBody();
   creatAthead(); //創建一個表頭
-  cursorGetData(db,'customers');
+  cursorGetData(db,storeName);
 };
 
 request.onerror = function(event) {
@@ -30,8 +34,8 @@ request.onerror = function(event) {
 function inData(){          
       //var db = event.target.result;
 
-      var transaction = db.transaction(['customers'], 'readwrite');
-      var objectStore = transaction.objectStore('customers');
+      var transaction = db.transaction([storeName], 'readwrite');
+      var objectStore = transaction.objectStore(storeName);
 
       var inputElement = document.getElementById("stockNo");
       var inputStockName=document.getElementById("stockName");
@@ -45,13 +49,13 @@ function inData(){
       var transDate=transDate.value;
       var stockNum=stockNum.value;
 
-      objectStore.add({ id: inputValue , name: stockName,date: transDate, amount:transAmount, stocknum:stockNum  });          
+      objectStore.add({ stackNo: inputValue , name: stockName,date: transDate, amount:transAmount, stocknum:stockNum  });          
 
       transaction.oncomplete = function() {
       console.log('已經存入資料!');
       clearTBody();
-      creatAthead(); //創建一個表頭
-      cursorGetData(db,'customers');
+      //creatAthead(); //創建一個表頭
+      cursorGetData(db,storeName);
       };
 
       transaction.onerror = function(event) {
@@ -62,24 +66,26 @@ function inData(){
 // 查询数据
 function lookUpData()
 {    
-  var inputElement = document.getElementById("stockName");
-  var indexValue = inputElement.value;    
-  var storeName='customers';
-  var indexName='name';
+  var inputElement = document.getElementById("stockNo");
+  var indexValue = inputElement.value;      
+  if(indexValue=='') {
+     inputElement = document.getElementById("stockName");
+     indexValue = inputElement.value;      
+     indexName='name';
+  }    
+  else  var indexName='id';
   var store = db.transaction([storeName], "readwrite").objectStore(storeName); // 仓库对象
   var request=store.index(indexName).get(indexValue);  
+  //var request=store.get(1);
+  
+  //console.log(request);
   request.onerror = function () {
-    console.log("查詣失敗");
+    console.log("查詢失敗");
   };
   request.onsuccess = function (even) {
-    var result = even.target.result;
-    console.log("索引查詢結果:", result);
-  }; 
-
-  //creatAthead(); //創建一個表頭
-  clearTBody();
-  creatAthead(); //創建一個表頭
-  cursorGetData(db,storeName);
+    var result = even.target.result;    
+    clearTBody();    
+  };   
   cursorGetDataByIndex(db, storeName, indexName, indexValue); 
 } 
 
@@ -96,81 +102,77 @@ function lookUpData()
   var request = store.openCursor(); // 指针对象
   // 游标开启成功，逐行读数据
   request.onsuccess = function (e) {
-    var cursor = e.target.result;
-
-    //findStock.innerHTML="";
-    
+    var cursor = e.target.result;    
     if (cursor) {
       // 必须要检查
       data.push(cursor.value);     
       cursor.continue(); // 遍历了存储对象中的所有内容
-    } else {      
-    //var displayDiv = document.getElementById("findStock");      
-    //data.forEach(function(item) { 
-    //  var p = document.createElement("p");
-    //  p.textContent =
-    //  " " + item.id + 
-    //  " " + item.name +
-    //  " " + item.date +
-    //  " " + item.amount +
-    //  " " + item.stocknum; 
-    //  displayDiv.appendChild(p);                                
-    //}); 
+    } else {         
       creatTBody(data);      
     }    
   }          
 }
 function creatTBody(data)
-{
-    var table = document.getElementById("stockTable");       
- 
+{  
+  //var tbody = document.getElementById("stockTbody").getElementsByTagName('tbody')[0];
+  var tbody = document.getElementById("stockTbody")
+
   data.forEach(function(item) {
-    
-    var row = document.createElement('tr');
-    
-    var cell = document.createElement('td');
-    cell.textContent = item.id; row.appendChild(cell);
-    var cell = document.createElement('td');
-    cell.textContent = item.name; row.appendChild(cell);
-    var cell = document.createElement('td');
-    cell.textContent = item.date; row.appendChild(cell);
-    var cell = document.createElement('td');
-    cell.textContent = item.amount; row.appendChild(cell);
-    var cell = document.createElement('td');
-    cell.textContent = item.stocknum; row.appendChild(cell);
-    var cell = document.createElement('td');
-    cell.textContent = item.amount*item.stocknum;  row.appendChild(cell);
-    
-    table.appendChild(row);
-    });
-}
+  //console.log(item);
+  var newRow = tbody.insertRow();
+
+  var checkboxCell = newRow.insertCell(0);
+  var stock1 = newRow.insertCell(1);
+  var stock2 = newRow.insertCell(2);
+  var stock3 = newRow.insertCell(3);
+  var stock4 = newRow.insertCell(4);
+  var stock5 = newRow.insertCell(5);
+  var stock6 = newRow.insertCell(6);
+
+  var checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.name = "checkbox" + (tbody.rows.length);
+
+  var label = document.createElement("label");
+  label.appendChild(checkbox);
+  //label.appendChild(document.createTextNode("Option " + (tbody.rows.length)));
+
+  checkboxCell.appendChild(label);
+  stock1.textContent = item.stackNo;
+  stock2.textContent = item.name;
+  stock3.textContent = item.date;
+  stock4.textContent = item.amount;
+  stock5.textContent = item.stocknum;
+  stock6.textContent = item.amount*item.stocknum;
+  })
+} 
 function clearTBody() 
 {
-  var table=document.getElementById('stockTable');
-  table.innerHTML='';
+  //var table=document.getElementById('stockTable');
+  //table.innerHTML='';
+    var table=document.getElementById('stockTbody');
+    table.innerHTML='';
 }
-  
 function creatAthead()
 {    
-   var table = document.getElementById("stockTable");    
-   var newHeaders = [        
-     "股票代號",
-     "股票名稱",
-     "交易日期",
-     "交易價格",
-     "交易股數",
-     "交易總價"
-   ];       
-   //        
-   var headerRow = document.createElement('tr');
-   // 新增每個表頭 
-   newHeaders.forEach(function(headerText) {
-     var cell = document.createElement('th');
-     cell.textContent=headerText;     
-     headerRow.appendChild(cell);           
-     table.appendChild(headerRow);
-   }); 
+   var table = document.getElementById("stockTable");   
+   var newHeaders = [ "股票代號","股票名稱","交易日期","交易價格","交易股數","交易總價"  ];    
+    // 新增每個表頭 
+    var headerRow=table.insertRow();
+    var checkboxCell=headerRow.insertCell(0);    
+    var checkbox=document.createElement('input');
+    checkbox.type='checkbox';
+    checkbox.name='checkall';
+    var label=document.createElement("label");
+    label.appendChild(checkbox);
+    label.appendChild(document.createTextNode("全選" ));
+    checkboxCell.appendChild(label);
+    for( var i=1;i<7;i++){
+      var cell=headerRow.insertCell(i);
+      cell.textContent=newHeaders[i-1];
+    }    
 }
+
  /**
  * 通过索引和游标查询记录
  * @param {object} db 数据库实例
@@ -184,14 +186,19 @@ function creatAthead()
   var request = store
     .index(indexName) // 索引对象
     .openCursor(IDBKeyRange.only(indexValue)); // 指针对象
+    
   request.onsuccess = function (e) {
     var cursor = e.target.result;
+    list=[];
     if (cursor) {
       // 必须要检查
-      list.push(cursor.value);
+      list.push(cursor.value);           
+      creatTBody(list);
       cursor.continue(); // 遍历了存储对象中的所有内容
     } else {
-      console.log("游标索引查询结果：", list);
+      console.log("查詢完成");
+      //console.log("游标索引查询结果：", list);       
+      //creatTBody(list);
     }
   };
   request.onerror = function (e) {};
@@ -200,7 +207,7 @@ function creatAthead()
 function upData(){
 // 更新数据
   //var db = event.target.result;
-  var updateTransaction = db.transaction(['customers'], 'readwrite');
+  var updateTransaction = db.transaction([storeName], 'readwrite');
   var updateObjectStore = updateTransaction.objectStore('customers');
 
   //var updatedCustomer = { id: '00929', name: '台灣50' };
